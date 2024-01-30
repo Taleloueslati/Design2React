0d4daa0ee06ad02f84658663647783094e1d8581
Taleloueslati <taleloueslati28@gmail.com>
2024-01-30 15:53:35 +0100

    Figma To React

---
+++ src/pages/HomePage.js
@@ -0,0 +1,96 @@
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <img
+        className="city-buildings-night-2-icon"
+        alt=""
+        src="/citybuildingsnight-2@2x.png"
+      />
+      <div className="home-page-child" />
+      <img className="fpdl-1-icon" alt="" src="/-fpdl-1@2x.png" />
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <div className="home-page-item" />
+      <b className="breaking-news">BREAKING NEWS</b>
+      <div className="we-are-live">WE ARE LIVE!</div>
+      <div className="we-are-live1">WE ARE LIVE!</div>
+      <div className="we-are-live2">WE ARE LIVE!</div>
+      <div className="we-are-live3">WE ARE LIVE!</div>
+      <div className="we-are-live4">WE ARE LIVE!</div>
+      <div className="we-are-live5">WE ARE LIVE!</div>
+      <div className="we-are-live6">WE ARE LIVE!</div>
+      <div className="we-are-live7">WE ARE LIVE!</div>
+      <img
+        className="arrow-caret-up-md"
+        alt=""
+        src="/arrow--caret-up-md@2x.png"
+      />
+      <img
+        className="sans-titre-1er-1"
+        alt=""
+        src="/sans-titre--1er-1@2x.png"
+      />
+      <b className="we-are-one">{`We are one people but we live as if `}</b>
+      <b className="divided-everything">Divided … everything is connected</b>
+      <div className="rectangle-parent">
+        <div className="group-child" />
+        <div className="group-item" />
+        <div className="group-inner" />
+        <div className="listen-live">LISTEN LIVE</div>
+        <div className="contact-us">CONTACT US</div>
+        <div className="get-the-app">GET THE APP</div>
+        <img className="rectangle-icon" alt="" src="/rectangle-148@2x.png" />
+        <div className="greyscale-shot-beautiful-city-" />
+        <img className="group-child1" alt="" src="/rectangle-149@2x.png" />
+        <img className="group-child2" alt="" src="/rectangle-150@2x.png" />
+        <img className="group-child3" alt="" src="/rectangle-151@2x.png" />
+        <img
+          className="arrow-caret-circle-up"
+          alt=""
+          src="/arrow--caret-circle-up@2x.png"
+        />
+        <img
+          className="arrow-caret-circle-up1"
+          alt=""
+          src="/arrow--caret-circle-up@2x.png"
+        />
+        <div className="rectangle-div" />
+        <b className="follow-for-more">FOLLOW FOR MORE</b>
+        <b className="follow-for-more1">FOLLOW FOR MORE</b>
+        <b className="follow-for-more2">FOLLOW FOR MORE</b>
+        <b className="follow-for-more3">FOLLOW FOR MORE</b>
+        <b className="follow-for-more4">FOLLOW FOR MORE</b>
+        <b className="follow-for-more5">FOLLOW FOR MORE</b>
+        <b className="follow-for-more6">FOLLOW FOR MORE</b>
+        <b className="foll">FOLL</b>
+        <img
+          className="iconmonstr-youtube-8-1"
+          alt=""
+          src="/iconmonstryoutube8-1.svg"
+        />
+        <img
+          className="iconmonstr-instagram-13-1"
+          alt=""
+          src="/iconmonstrinstagram13-1.svg"
+        />
+        <img
+          className="iconmonstr-facebook-6-1-1"
+          alt=""
+          src="/iconmonstrfacebook6-1-1.svg"
+        />
+        <div className="group-child4" />
+        <div className="group-child5" />
+        <b className="track-matrak">TRACK MATRAK</b>
+        <div className="div">20.12.203</div>
+        <img
+          className="media-play-circle"
+          alt=""
+          src="/media--play-circle.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
